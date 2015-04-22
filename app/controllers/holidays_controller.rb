33ab70fdb74f959b00475d59749d36464649b37d
hollywilsonbyrne <hollywilsonbyrne@gmail.com>
2015-04-22 18:23:50 +0000

    csv upload and form tidy up

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -30,7 +30,7 @@ class HolidaysController < ApplicationController
 
     respond_to do |format|
       if @holiday.save
-        format.html { redirect_to @holiday, notice: 'Holiday was successfully created.' }
+        format.html { redirect_to @holiday, notice: 'Holiday was successfully requested.' }
         format.json { render :show, status: :created, location: @holiday }
       else
         format.html { render :new }
@@ -44,7 +44,7 @@ class HolidaysController < ApplicationController
   def update
     respond_to do |format|
       if @holiday.update(holiday_params)
-        format.html { redirect_to @holiday, notice: 'Holiday was successfully updated.' }
+        format.html { redirect_to @holiday, notice: 'Holiday request was successfully updated.' }
         format.json { render :show, status: :ok, location: @holiday }
       else
         format.html { render :edit }
@@ -58,7 +58,7 @@ class HolidaysController < ApplicationController
   def destroy
     @holiday.destroy
     respond_to do |format|
-      format.html { redirect_to holidays_url, notice: 'Holiday was successfully destroyed.' }
+      format.html { redirect_to holidays_url, notice: 'Holiday request was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420154017) do
+ActiveRecord::Schema.define(version: 20150421221301) do
+
+  create_table "departments", force: true do |t|
+    t.string   "name"
+    t.string   "position"
+    t.string   "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "holidays", force: true do |t|
     t.date     "dateTo"
